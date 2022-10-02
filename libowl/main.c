53a95d12b9006b688facadca5c9389cc49f419bd
Rahul Gupta <rahulr09gupta@gmail.com>
2022-10-02 10:49:10 +0530

    Initial Commit

---
+++ libowl/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  libowl
+//
+//  Created by Rahul Gupta on 02/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
