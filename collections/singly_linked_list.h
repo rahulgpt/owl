16351ca5d4ddc6ac1fbd3e0aac30e74301d1f127
Rahul Gupta <rahulr09gupta@gmail.com>
2023-01-06 22:45:37 +0530

    chore: substitute elfree to el_free in sll.h

--- collections/singly_linked_list.h
+++ collections/singly_linked_list.h
@@ -12,7 +12,7 @@ typedef struct Node
 
 typedef struct SinglyLinkedList owl_sll_t;
 
-owl_sll_t *owl_sll_init(size_t size, void (*elfree)(void *data));
+owl_sll_t *owl_sll_init(size_t size, void (*el_free)(void *data));
 void owl_sll_free(owl_sll_t *list);
 void owl_sll_binsert(owl_sll_t *list, void *data);
 void *owl_sll_bremove(owl_sll_t *list);
